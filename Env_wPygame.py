4a897a7eaa7e63c408ed6292147cc49b807a3266
LucasColas <lucas.colas13@gmail.com>
2021-09-04 15:06:18 +0200

    update

--- Env_wPygame.py
+++ Env_wPygame.py
@@ -1,9 +1,32 @@
 import pygame
 import numpy as np
 
+WIN_Size = (600,600)
+
 class Blob:
     def __init__(self, Size):
         self.x = np.random.randint(0, Size)
         self.y = np.random.randint(0, Size)
 
-    
+    def __str__(self):
+        return f"{self.x}, {self.y}"
+
+    def __sub__(self, other):
+        return (self.x - other.x, self.y - other.y)
+
+    def action(self, choice):
+        if choice == 0:
+            self.move(-1,0)
+
+        elif choice == 1:
+            self.move(1,0)
+
+        elif choice == 2:
+            self.move(0,-1)
+
+        elif choice == 3:
+            self.move(0,1)
+
+
+    def move(self, x=False,y=False):
+        pass
