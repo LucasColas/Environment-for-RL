5ccac5eebf8fb5a8359b7594baaf7aa2ec2cfc64
LucasColas <lucas.colas13@gmail.com>
2021-09-05 15:25:20 +0200

    update

--- render_env.py
+++ render_env.py
@@ -1,11 +1,16 @@
 import pygame
+pygame.init()
+
 from Env_wpygame import *
 from Q_Learning import *
 
 
 def main():
     Size = (600,600)
+
     color = {"blue": (20, 81, 232), "red": (238, 34, 24), "green":( 0, 252, 8)}
     Player = Blob(color["blue"], Size[0], Size[0]//15, 15)
     Enemy = Blob(color["red"], Size[0], Size[0]//15, 15)
     Food = Blob(color["green"], Size[0], Size[0]//15, 15)
+
+    
