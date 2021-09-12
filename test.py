95f98823cf39455ffaefaa74502d23c58cc32b28
LucasColas <lucas.colas13@gmail.com>
2021-09-12 10:02:58 +0200

    update

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from Q_Learning import create_Q_Table, binning
+import sys
 
 
 Width_chunk, Height_chunk = binning((600,600),20)
@@ -6,4 +7,5 @@ Width_chunk, Height_chunk = binning((600,600),20)
 
 Q_Table = create_Q_Table(Width_chunk, Height_chunk)
 print(Q_Table)
-print(f"len : {Q_Table}")
+print("len : ", len(Q_Table))
+print(sys.getsizeof(object))
